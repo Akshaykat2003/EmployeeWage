c4ccf18fafb3ac201bdcf609256244d994d57830
akshay <akshaykatoch38@gmail.com>
2024-12-04 21:36:59 +0530

    Updated Employee Daily wage logic

--- UC2.rb
+++ UC2.rb
@@ -5,12 +5,8 @@ puts "UC2: Calculate Daily Employee Wage"
 
 wagePerHour = 20
 fullDayHour = 8
-def calculateDailyWage(wagePerHour, fullDayHour)
 
-  dailyWage = wagePerHour * fullDayHour
-  return dailyWage
-end
+dailyWage = wagePerHour * fullDayHour
 
-dailyWage = calculateDailyWage(wagePerHour,fullDayHour)
-puts "Daily wage : #{dailyWage}"
+puts "Daily Employee Wage : $#{dailyWage}"
 
