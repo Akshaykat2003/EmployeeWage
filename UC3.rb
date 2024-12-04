9392e53a6025b776b2f9bf8629f50d8a24bb0bbf
akshay <akshaykatoch38@gmail.com>
2024-12-04 21:41:27 +0530

    Updated part time Employee and  wage logic

--- UC3.rb
+++ UC3.rb
@@ -5,12 +5,6 @@ puts "UC3: Add Part time Employee & Wage"
 
 wagePerHour = 20
 partTimeHour = 4
-
-def partTimeWage(wagePerHour,partTimeHour)
-  partTimeWage = wagePerHour * partTimeHour
-  return partTimeWage
-end
-
-partTimeWage=partTimeWage(wagePerHour,partTimeHour)
-puts "part Time wage : #{partTimeWage}"
+partTimeWage = wagePerHour * partTimeHour
+puts "part Time wage : $#{partTimeWage}"
 
